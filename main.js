e0bb3e25430ea58b72ddb1bddcf823c7e74713d7
Gabriela Plasencia <gabyplasencia1995@gmail.com>
2024-08-05 17:10:58 +0100

    added empty cart section styles and added button to increment and decrement quantities

--- main.js
+++ main.js
@@ -21,20 +21,50 @@ fetch("./data.json")
             }
             checkSize();
 
-            window.addEventListener('resize', checkSize);
+            window.addEventListener("resize", checkSize);
 
-            const cartBtn = document.createElement("button");
-            cartBtn.id = card.id;
-            cartBtn.className = "product__cart-btn";
-            cartBtn.innerHTML = "Add to Cart";
-            wrapperImg.appendChild(cartBtn);
+            const cartBtnAdd = document.createElement("button");
+            cartBtnAdd.classList.add("product__cart-btn", "product__cart-btn-add");
+            cartBtnAdd.innerHTML = "Add to Cart";
+            wrapperImg.appendChild(cartBtnAdd);
 
             const cartIcon = document.createElement("img");
             cartIcon.className = "cart-icon";
             cartIcon.src = "./assets/images/icon-add-to-cart.svg";
             cartIcon.alt = "shopping cart icon";
             cartIcon.setAttribute("aria-hidden", "true");
-            cartBtn.appendChild(cartIcon);
+            cartBtnAdd.appendChild(cartIcon);
+
+            const cartBtnQuantity = document.createElement("div");
+            cartBtnQuantity.classList.add("product__cart-btn", "product__cart-btn-quantity");
+            cartBtnQuantity.id = card.id;
+            wrapperImg.appendChild(cartBtnQuantity);
+
+            const cartBtnSum = document.createElement("button");
+            cartBtnSum.className = "plus-item";
+            const plusIcon = document.createElement("img");
+            plusIcon.className = "plus-icon";
+            plusIcon.src = "./assets/images/icon-increment-quantity.svg";
+            plusIcon.alt = "add one more item";
+            cartBtnSum.appendChild(plusIcon);
+
+            const itemQuantity = document.createElement("span");
+            itemQuantity.className = "quantity-item";
+            itemQuantity.innerHTML = "0";
+
+            const cartBtnRest = document.createElement("button");
+            cartBtnRest.className = "minus-item";
+            const minusIcon = document.createElement("img");
+            minusIcon.className = "minus-icon";
+            minusIcon.src = "./assets/images/icon-decrement-quantity.svg";
+            minusIcon.alt = "take out one item";
+            cartBtnRest.appendChild(minusIcon);
+
+            cartBtnQuantity.appendChild(cartBtnRest);
+            cartBtnQuantity.appendChild(itemQuantity);
+            cartBtnQuantity.appendChild(cartBtnSum);
+
+
 
             const category = document.createElement("h3");
             category.className = "product__category";
